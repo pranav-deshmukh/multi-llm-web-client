2354a60445ef0fa8fb9d8075372e2cf23c98d7c3
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2025-07-16 11:21:18 +0530

    init frontend and backend logic

---
+++ app/api/chat/route.ts
@@ -0,0 +1,17 @@
+import { openai } from '@ai-sdk/openai';
+import { anthropic } from '@ai-sdk/anthropic';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: anthropic('claude-3-haiku-20240307'),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
